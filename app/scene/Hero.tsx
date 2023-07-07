612a43940504b1e8e3e7541fcc7333c14cace5af
Faisal Usman <faisalusman390@gmail.com>
2023-07-07 19:11:33 +0500

    use meemo

--- app/scene/Hero.tsx
+++ app/scene/Hero.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import { textVariant } from '../utils/motion'
 import Image from 'next/image'
